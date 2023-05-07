5da6b2fc411e4cb05e2c19b0d180f629cbd5f19c
DimasMukhlasIbrahim <dimasmuklas123@gmail.com>
2023-05-07 15:05:29 +0700

    membuat web kedai kopi kenangan senja

---
+++ main.js
@@ -0,0 +1,18 @@
+// console.log ($(document).ready(function(){
+// 	$('.tombol').click(function(){
+// 		$('.navbar-nav').toggleClass("slide-navbar-nav-tampil");
+// 	});
+// }));
+
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+// toogle class active
+// const navbarnav =document.querySelector ('.navbar-nav');
+
+// ketika di klik
+// document.querySelector ('#hamburger').
+// onclick= () => {
+//     navbarnav.classList.toggle ()
+// }
+
+alert ('hai, welcome to my website')
